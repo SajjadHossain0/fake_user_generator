100c0127026aadd6c38ed1365cf4a40d90847c72
Clock <mdsajjadhossain614@gmail.com>
2024-07-28 20:06:59 +0600

    new commit

--- src/main/java/com/fake_user_generator/Entities/User.java
+++ src/main/java/com/fake_user_generator/Entities/User.java
@@ -1,14 +1,14 @@
 package com.fake_user_generator.Entities;
 
-import jakarta.persistence.Entity;
+/*import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.Id;*/
 
-@Entity
+//@Entity
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String identifier;
     private String name;
