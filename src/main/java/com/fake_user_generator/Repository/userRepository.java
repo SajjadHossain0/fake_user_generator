751521ca494448d4f653e5bb6df52b203fdd2a94
Clock <mdsajjadhossain614@gmail.com>
2024-07-28 13:40:08 +0600

    random generator

---
+++ src/main/java/com/fake_user_generator/Repository/userRepository.java
@@ -0,0 +1,9 @@
+package com.fake_user_generator.Repository;
+
+import com.fake_user_generator.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface userRepository extends JpaRepository<User, Long> {
+}
